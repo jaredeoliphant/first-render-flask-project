6be5bf482b68439d4c2a05193d54f2d5dff7bb18
jaredeoliphant <43820309+jaredeoliphant@users.noreply.github.com>
2022-12-21 07:56:54 -0800

    wtforms and more links on home page

--- app.py
+++ app.py
@@ -1,29 +1,71 @@
-from flask import Flask, render_template,request,Response
+from flask import Flask, render_template, request, Response, session, redirect, flash, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField, FileField
+from wtforms.validators import Length, DataRequired, NumberRange
 from werkzeug.utils import secure_filename
 from data_process import data_process
-import os
-from matplotlib.figure import Figure
-from io import BytesIO
-import base64
+import os, time
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['UPLOAD_FOLDER'] = 'static\\files'
 
 
-@app.route("/speed_result", methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      start = request.form['starttime']
-      end = request.form['endtime']
-      secfilename = secure_filename(f.filename)
-      f.save(secfilename)
-      processed_values = data_process(secfilename,start,end)
-      os.remove(secfilename)
-      return render_template('speed_result.html',
+class DataForm(FlaskForm):
+
+    file = FileField('CSV File  ', validators=[DataRequired()])
+    start = FloatField('Start time for sampling bias calculation:  ',
+                       default=7.0, validators=[DataRequired(), NumberRange(min=0, max=10)])
+    end = FloatField('End time for sampling bias calculation:  ',
+                     default=9.8, validators=[DataRequired(), NumberRange(min=0, max=10)])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = DataForm()
+
+    if form.validate_on_submit():
+        f = form.file.data
+
+        filename = secure_filename(f.filename)
+        filepath = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], filename)
+
+        # delete all existing files in the upload folder to keep it clean
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'])
+        for ff in os.listdir(path):
+            fff = os.path.join(path, ff)
+            os.remove(fff)
+
+        # save the new file in the upload folder
+        f.save(filepath)
+
+        session['filename'] = filename.split('.csv')[0]
+        session['filepath'] = filepath
+        session['start'] = form.start.data
+        session['end'] = form.end.data
+
+        return redirect(url_for('speed_result'))
+
+    return render_template('index.html', form=form)
+
+
+@app.route("/speed_result", methods=['GET', 'POST'])
+def speed_result():
+    if request.method == 'GET':
+        filepath = session['filepath']
+        start = session['start']
+        end = session['end']
+
+        processed_values = data_process(filepath, start, end)
+        os.remove(filepath)
+
+        session['outputfilename'] = processed_values['outputfilename']
+
+        return render_template('speed_result.html',
                                testID=processed_values['testID'],
                                speed_kmh=processed_values['speed_kmh'],
                                imgdata=processed_values['imgdata'],
@@ -35,59 +77,38 @@ def upload_file():
                                starttime=start,
                                endtime=end
                                )
-      #except:
-       #   os.remove(secfilename)
-        #  return 'failed'   
-   else:
-       return 'no file'
+    else:
+        return 'did not work'
    
     
-@app.route("/getCSV/<outputfilename>")
-def getCSV(outputfilename):
-    with open(f'{outputfilename}.csv') as fp:
+@app.route("/getCSV")
+def getCSV():
+    fullfilepath = f'{session["outputfilename"]}.csv'
+    with open(fullfilepath) as fp:
         csv = fp.read()
-    #csv = '1,2,3\n4,5,6\n'
     
     return Response(
         csv,
         mimetype="text/csv",
         headers={"Content-disposition":
-                  f"attachment; filename={outputfilename}.csv"})
+                  f"attachment; filename={session['filename']}_OFFSET.csv"})
 
 
-
-@app.route("/plot")
-def hello():
-    # Generate the figure **without using pyplot**.
-    fig = Figure()
-    ax = fig.subplots()
-    ax.plot([1, 2])
-    # Save it to a temporary buffer.
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    #print(data)
-    return f"<img src='data:image/png;base64,{data}'/>"
-
-
-
-@app.route('/for_fun')
+@app.route('/ip_notebook',methods=['GET'])
 def ip_notebook():
-    return render_template('notebook.html')
+    return render_template('ip_notebook.html')
 
 
-@app.route('/example')
-def example():
+@app.route('/result_example',methods=['GET'])
+def result_example():
     return render_template('result_example.html')
 
 
-@app.route('/algorithm')
+@app.route('/algorithm',methods=['GET'])
 def algorithm():
-    return render_template('testing.html')
-
+    return render_template('algorithm.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
     
--- data_process.py
+++ data_process.py
@@ -6,8 +6,7 @@ from io import BytesIO
 import base64
 
 
-
-def data_process(filename,starttime,endtime):
+def data_process(filename, starttime, endtime):
     
     try:
         starttime = float(starttime)
@@ -16,35 +15,29 @@ def data_process(filename,starttime,endtime):
         starttime = 7
         endtime = 9.8
     
-    ## output file with suffix added as well as a log file for storing speed calculation
+    # output file with suffix added as well as a log file for storing speed calculation
     outputfilename = filename.split('.csv')[0] + '_OFFSET'
-    # logfilename = filename.split('.csv')[0] + '_log.txt'
-    
-    ## read the csv file in first to just get the testID,sampleRate,and channel information
+
+    # read the csv file in first to just get the testID,sampleRate,and channel information
     headerdata = pd.read_csv(filename,nrows=50)
-    # headerdata = pd.read_csv('298-064_FILTERED.csv')
-    
-    ## clean up
+
+    # clean up
     headerdata.dropna(axis=1,how='all',inplace=True)
     header = headerdata.iloc[:21]
     
-    ## extract useful data
+    # extract useful data
     testID = headerdata.iloc[2][1]
     sampleRate = int(headerdata.iloc[4][1])
     channels = headerdata.iloc[8][1:8]
     
-    ## read in the csv file again with only the meat of the data after the header section
+    # read in the csv file again with only the meat of the data after the header section
     data = pd.read_csv(filename,skiprows=22) 
+
+    # clean up
+    data.drop('Unnamed: 8',inplace=True,axis=1)
     
-    
-    ## clean up
-    data.drop('Unnamed: 8',inplace=True,axis=1) 
-    
-    #print(data.info())
-    #print(data.dtypes)
-    
-    ## sort the columns as needed to get them into the following order:
-    ## X accel, Y accel, Z accel, Roll, Pitch, Yaw
+    # sort the columns as needed to get them into the following order:
+    # X accel, Y accel, Z accel, Roll, Pitch, Yaw
     channel_list = headerdata.iloc[8][1:] ## channel descriptions [0,4,5,6,1,2,3]
     current_columns = data.columns.to_list()
     cur_columns_header = header.columns.to_list()
@@ -73,11 +66,10 @@ def data_process(filename,starttime,endtime):
         new_header_col.append(cur_columns_header[1+sortindex[x]])
         # print(x,new_columns,new_header_col)
     
-    ## use the new column list to reorder (reindex) the header dataframe and the data dataframe
+    # use the new column list to reorder (reindex) the header dataframe and the data dataframe
     data = data.reindex(columns=new_columns)
     header = header.reindex(columns=new_header_col)
-    
-    
+
     endd = 16750
     time_arr = data['Time'].iloc[0:endd].to_numpy()
     speed_arr = data['Chan 0:SPEED SENSOR'].iloc[0:endd].to_numpy()
@@ -88,21 +80,18 @@ def data_process(filename,starttime,endtime):
     speed_leading = factor*30008*3600/1000/(peaks[2]-peaks[0])
     speed_falling = factor*30008*3600/1000/(peaks[3]-peaks[1])
     offset = 3.6/speed_leading
-    
 
-    ## plot raw speed sensor data for user to confirm nothing is fishy
+    # plot raw speed sensor data for user to confirm nothing is fishy
     
     xlim_param = resampled_time[peaks[0]] - 0.03275 + .03+.0017
     
-    ## plot raw speed sensor data for user to confirm nothing is fishy
+    # plot raw speed sensor data for user to confirm nothing is fishy
     fig = Figure(figsize=(16,6))
     ax1 = fig.add_subplot(121)
     
     ax1.plot(time_arr,speed_arr,'^:',label='original')
     ax1.plot(time_arr-offset,speed_arr,'>:',label='shifted')
-    
-    #xlim_param = resampled_time[peaks[0]] - 0.03275 + .03+.0017
-    #print(xlim_param)
+
     ax1.set_xlim(xlim_param,xlim_param+.01)
     
     ax1.legend()
@@ -110,8 +99,7 @@ def data_process(filename,starttime,endtime):
     ax1.set_title(f'speed = {round(speed_leading,2)} kmh')
     ax1.set_xlabel('Time (s)')
     ax1.set_ylabel('Speed Sensor')
-    
-    
+
     ax2 = fig.add_subplot(122)
     
     ax2.plot(data['Time'],data.iloc[:,5],label='Roll')
@@ -121,36 +109,30 @@ def data_process(filename,starttime,endtime):
     ylimits = ax2.get_ylim()
     ax2.plot([starttime]*2,ylimits,'k:')
     ax2.plot([endtime]*2,ylimits,'k:')
-    
-    
+
     ax2.set_xlabel('Time (s)')
     ax2.set_ylabel('Angular rates')
     
     ax2.legend()
     ax2.set_ylim(ylimits)
     ax2.grid()
-    
-    
+
     buf = BytesIO()
     fig.savefig(buf, format="png")
     # Embed the result in the html output.
     imgdata = base64.b64encode(buf.getbuffer()).decode("ascii")
     
-    # get from html eventually
-    #starttime=7
-    #endtime=10
-    
     datasubset = data[(data['Time'] >= starttime) & (data['Time'] <= endtime)]
     
-    ## take a mean (average) of the three vectors
+    # take a mean (average) of the three vectors
     rollbias = datasubset.iloc[:,5].mean()
     pitchbias = datasubset.iloc[:,6].mean()
     yawbias = datasubset.iloc[:,7].mean()
     
-    ## copy of the data is required so that it can be modified
+    # copy of the data is required so that it can be modified
     offsetdata = data.copy()
     
-    ## subract sampling bias from data
+    # subract sampling bias from data
     offsetdata.iloc[:,5] = offsetdata.iloc[:,5] - rollbias
     offsetdata.iloc[:,6] = offsetdata.iloc[:,6] - pitchbias
     offsetdata.iloc[:,7] = offsetdata.iloc[:,7] - yawbias
@@ -158,22 +140,19 @@ def data_process(filename,starttime,endtime):
     # data.to_csv("output.csv")
     header.to_csv(outputfilename+'.csv',index=False,header=['Headers','','','','','','',''])
     offsetdata.to_csv(outputfilename+'.csv',index=False,mode='a')
-    
-    
-    return {'speed_kmh':speed_leading,
-            'speed_falling':speed_falling,
-            #'xlim_param':xlim_param,
-            #'offset':offset,
-            'testID':testID,
-            #'sampleRate':sampleRate,
-            'imgdata':imgdata,
-            'outputfilename':outputfilename,
-            'rollbias':rollbias,
-            'pitchbias':pitchbias,
-            'yawbias':yawbias
+
+    return {'speed_kmh': speed_leading,
+            'speed_falling': speed_falling,
+            'testID': testID,
+            'imgdata': imgdata,
+            'outputfilename': outputfilename,
+            'rollbias': rollbias,
+            'pitchbias': pitchbias,
+            'yawbias': yawbias
             }
 
-def resample_signal(t_arr,x_arr,factor=2):
+
+def resample_signal(t_arr, x_arr, factor=2):
 
     length = len(t_arr)
     idx = pd.date_range("2018-01-01", periods=length, freq="H")
